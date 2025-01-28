ae36745db985ee62506488c315e6afe71b44e6e6
Vision295 <theophilewachtel@gmail.com>
2025-01-28 16:24:26 +0100

    v2 avec parser

--- elm/src/LearnHtml.elm
+++ elm/src/LearnHtml.elm
@@ -1,42 +1,55 @@
-module LearnHtml exposing (main)
+module LearnHtml exposing (..)
 
 import Browser
-import Html exposing (Html, div, input, text)
+import Html exposing (Html, button, div, input)
 import Html.Attributes exposing (placeholder, value)
-import Html.Events exposing (onInput)
-import Svg exposing (..)
+import Html.Events exposing (onClick, onInput)
+import Parser exposing ((|.), (|=), Parser, Trailing(..))
+import Svg
 import Svg.Attributes exposing (..)
 
 -- MODEL
 type alias Model =
     { inputText : String
+    , words : List String
     }
 
 init : Model
 init =
     { inputText = ""
+    , words = []
     }
 
 -- UPDATE
 type Msg
     = UpdateText String
+    | ParseWords
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         UpdateText newText ->
             { model | inputText = newText }
+            
+        ParseWords ->
+            { model | words = parseText model.inputText }
+
+-- PARSER
+parseText : String -> List String
+parseText input =
+    String.split " " input
+        |> List.filter (not << String.isEmpty)
 
 -- VIEW
 view : Model -> Html Msg
 view model =
     div []
-        [ svg 
+        [ Svg.svg 
             [ width "120"
             , height "120"
             , viewBox "0 0 120 120"
             ] 
-            [ rect 
+            [ Svg.rect 
                 [ x "10"
                 , y "10"
                 , width "100"
@@ -46,7 +59,7 @@ view model =
                 , fill "blue"
                 ] 
                 []
-            , circle 
+            , Svg.circle 
                 [ cx "50"
                 , cy "50"
                 , r "50"
@@ -54,13 +67,23 @@ view model =
                 ] 
                 []
             ]
-        , Html.input
-            [ placeholder "Enter some text"
-            , value model.inputText
-            , onInput UpdateText
-            ] 
-            []
-        , Html.div [] [ Html.text model.inputText ]
+        , div []
+            [ Html.input
+                [ placeholder "Enter some text"
+                , value model.inputText
+                , onInput UpdateText
+                ] 
+                []
+            , button [ onClick ParseWords ] [ Html.text "Send" ]
+            ]
+        , div [] 
+            [ Html.text "Words found: "
+            , Html.text (String.fromInt (List.length model.words))
+            ]
+        , div [] 
+            [ Html.text "Words: "
+            , Html.text (String.join ", " model.words)
+            ]
         ]
 
 -- MAIN
